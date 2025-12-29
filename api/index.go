2b67ed83d9186246ab5698f350ebc2568c8c977f
hafidhuddien <m.hafid@ifca.co.id>
2025-12-29 13:09:32 +0700

    dep

--- api/index.go
+++ api/index.go
@@ -7,8 +7,13 @@ import (
 
 var ginEngine *gin.Engine
 
-func init() {
-    ginEngine = gin.Default()
+// func init() {
+
+// }
+
+// Vercel entry point
+func Handler(w http.ResponseWriter, r *http.Request) {
+        ginEngine = gin.Default()
 	ginEngine.GET("/", func(c *gin.Context) {
         c.JSON(200, gin.H{"message": "Hello from Gin on Vercel!"})
     })
@@ -18,9 +23,6 @@ func init() {
 	ginEngine.GET("/hello2", func(c *gin.Context) {
         c.JSON(200, gin.H{"message": "Hello2 from Gin on Vercel!"})
     })
-}
-
-// Vercel entry point
-func Handler(w http.ResponseWriter, r *http.Request) {
+    
     ginEngine.ServeHTTP(w, r)
 }
