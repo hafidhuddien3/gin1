254e7b885282c985c30ccc21f2f00a9e9d4a3bfc
hafidhuddien <m.hafid@ifca.co.id>
2025-12-29 14:55:57 +0700

    dep

--- api/main2.go
+++ api/main2.go
@@ -9,32 +9,33 @@ import (
     "github.com/gin-gonic/gin"
     "github.com/swaggo/gin-swagger"
     "github.com/swaggo/files"
+      "net/http"
 )
 
 // func main() {
 func HandlerMain(w http.ResponseWriter, r *http.Request) {
     db.InitDB()
 
-    r := gin.Default()
+    g := gin.Default()
 
-    r.GET("/", func(c *gin.Context) {
+    g.GET("/", func(c *gin.Context) {
     c.JSON(200, gin.H{
       "message": "ping",
     })
     })
 
-    r.GET("/ping", func(c *gin.Context) {
+    g.GET("/ping", func(c *gin.Context) {
     c.JSON(200, gin.H{
       "message": "ping",
     })
     })
 
     // Routes
-    r.POST("/books", handlers.CreateBook)
-    r.GET("/books", handlers.GetBooks)
-    r.GET("/books/:id", handlers.GetBook)
-    r.PUT("/books/:id", handlers.UpdateBook)
-    r.DELETE("/books/:id", handlers.DeleteBook)
+    g.POST("/books", handlers.CreateBook)
+    g.GET("/books", handlers.GetBooks)
+    g.GET("/books/:id", handlers.GetBook)
+    g.PUT("/books/:id", handlers.UpdateBook)
+    g.DELETE("/books/:id", handlers.DeleteBook)
 
 // get ingredient v
 // get product v
@@ -43,31 +44,31 @@ func HandlerMain(w http.ResponseWriter, r *http.Request) {
 // post product v
 // post product_ingredient v
 
-    r.POST("/ingredients", handlers.CreateIngredient)
-    r.GET("/ingredients", handlers.GetIngredients)
+    g.POST("/ingredients", handlers.CreateIngredient)
+    g.GET("/ingredients", handlers.GetIngredients)
     // r.GET("/ingredients/:id", handlers.GetIngredient)
-    r.PUT("/ingredients/:id", handlers.UpdateIngredient)
-    r.DELETE("/ingredients/:id", handlers.DeleteIngredient)
+    g.PUT("/ingredients/:id", handlers.UpdateIngredient)
+    g.DELETE("/ingredients/:id", handlers.DeleteIngredient)
 
-    r.POST("/products", handlers.CreateProduct)
-    r.GET("/products", handlers.GetProducts)
-    r.PUT("/products/:id", handlers.UpdateProduct)
-    r.DELETE("/products/:id", handlers.DeleteProduct)
+    g.POST("/products", handlers.CreateProduct)
+    g.GET("/products", handlers.GetProducts)
+    g.PUT("/products/:id", handlers.UpdateProduct)
+    g.DELETE("/products/:id", handlers.DeleteProduct)
 
-    r.POST("/productIngredients", handlers.CreateProductIngredient)
-    r.GET("/productIngredients/product/:productId", handlers.GetProductIngredientByProductId)
-    r.PUT("/productIngredients/:id", handlers.UpdateProductIngredient)
-    r.DELETE("/productIngredients/:id", handlers.DeleteProductIngredient)
+    g.POST("/productIngredients", handlers.CreateProductIngredient)
+    g.GET("/productIngredients/product/:productId", handlers.GetProductIngredientByProductId)
+    g.PUT("/productIngredients/:id", handlers.UpdateProductIngredient)
+    g.DELETE("/productIngredients/:id", handlers.DeleteProductIngredient)
 
     // Routes
-    r.POST("/pattern", handlers.SearchPattern)
+    g.POST("/pattern", handlers.SearchPattern)
 
     // Swagger
-    r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+    g.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
     // r.Run(":8080")
     // r.Run()
-    r.ServeHTTP(w, r)
+    g.ServeHTTP(w, r)
 }
 
 // func Handler(w http.ResponseWriter, r *http.Request) {
