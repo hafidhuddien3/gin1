973dcbe26e3520caec5af0030883edbdf9ba4da9
hafidhuddien <m.hafid@ifca.co.id>
2025-12-29 14:41:25 +0700

    dep

--- api/main2.go
+++ api/main2.go
@@ -65,6 +65,22 @@ func main() {
     r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
     // r.Run(":8080")
-    // r.Run()
-    r.ServeHTTP(w, r)
+    r.Run()
+    // r.ServeHTTP(w, r)
 }
+
+// func Handler(w http.ResponseWriter, r *http.Request) {
+//     var ginEngine *gin.Engine
+//     ginEngine = gin.Default()
+// 	  ginEngine.GET("/", func(c *gin.Context) {
+//         c.JSON(200, gin.H{"message": "Hello from Gin on Vercel!"})
+//     })
+//     // ginEngine.GET("/hello", func(c *gin.Context) {
+//     //     c.JSON(200, gin.H{"message": "Hello from Gin on Vercel!"})
+//     // })
+// 	  ginEngine.GET("/hello2", func(c *gin.Context) {
+//         c.JSON(200, gin.H{"message": "Hello2 from Gin on Vercel!"})
+//     })
+
+//     ginEngine.ServeHTTP(w, r)
+// }
