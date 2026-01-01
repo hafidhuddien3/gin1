a5e52e15d86ca680eea3c893d3a8e5dbe84834f5
hafidhuddien3 <shubuhshubuh@gmail.com>
2026-01-01 10:10:33 +0700

    dep

--- api/ingredients.go
+++ api/ingredients.go
@@ -34,32 +34,32 @@ func HandlerIngredients(w http.ResponseWriter, r *http.Request) {
 	// if r.Method == "POST" {
 	// } else {
 	// }
-	g.POST("/api/books", handlers.CreateBook)
-	g.GET("/api/books", handlers.GetBooks)
-    g.GET("/api/books/:id", handlers.GetBook)
-    g.PUT("/api/books/:id", handlers.UpdateBook)
-    g.DELETE("/api/books/:id", handlers.DeleteBook)
+	// g.POST("/api/books", handlers.CreateBook)
+	// g.GET("/api/books", handlers.GetBooks)
+  //   g.GET("/api/books/:id", handlers.GetBook)
+  //   g.PUT("/api/books/:id", handlers.UpdateBook)
+  //   g.DELETE("/api/books/:id", handlers.DeleteBook)
 
     g.POST("/api/ingredients", handlers.CreateIngredient)
     g.GET("/api/ingredients", handlers.GetIngredients)
     g.PUT("/api/ingredients/:id", handlers.UpdateIngredient)
     g.DELETE("/api/ingredients/:id", handlers.DeleteIngredient)
 
-    g.POST("/api/products", handlers.CreateProduct)
-    g.GET("/api/products", handlers.GetProducts)
-    g.PUT("/api/products/:id", handlers.UpdateProduct)
-    g.DELETE("/api/products/:id", handlers.DeleteProduct)
+    // g.POST("/api/products", handlers.CreateProduct)
+    // g.GET("/api/products", handlers.GetProducts)
+    // g.PUT("/api/products/:id", handlers.UpdateProduct)
+    // g.DELETE("/api/products/:id", handlers.DeleteProduct)
 
-    g.POST("/api/productIngredients", handlers.CreateProductIngredient)
-    g.GET("/api/productIngredients/product/:productId", handlers.GetProductIngredientByProductId)
-    g.PUT("/api/productIngredients/:id", handlers.UpdateProductIngredient)
-    g.DELETE("/api/productIngredients/:id", handlers.DeleteProductIngredient)
+    // g.POST("/api/productIngredients", handlers.CreateProductIngredient)
+    // g.GET("/api/productIngredients/product/:productId", handlers.GetProductIngredientByProductId)
+    // g.PUT("/api/productIngredients/:id", handlers.UpdateProductIngredient)
+    // g.DELETE("/api/productIngredients/:id", handlers.DeleteProductIngredient)
 
-    // Routes
-    g.POST("/api/pattern", handlers.SearchPattern)
+    // // Routes
+    // g.POST("/api/pattern", handlers.SearchPattern)
 
-    // Swagger
-    g.GET("/api/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+    // // Swagger
+    // g.GET("/api/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
     // r.Run(":8080")
     g.ServeHTTP(w, r)
