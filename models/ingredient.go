402d3ab3bafb5a016f7d70d9bacea5c845a5b9fa
hafidhuddien3 <shubuhshubuh@gmail.com>
2026-01-01 13:34:30 +0700

    fix model

--- models/ingredient.go
+++ models/ingredient.go
@@ -4,7 +4,7 @@ type Ingredient struct {
 	// ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4()" json:"id"`
     ID     uint   `json:"id" gorm:"primaryKey"`
     Name  string `json:"name"`
-    Price string `json:"price"`
+    Price int `json:"price"`
     Qty int `json:"qty"`
     QtyName string `json:"qtyName"`
 }
